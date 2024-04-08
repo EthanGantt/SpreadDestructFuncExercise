75a1cb91dbbadb60b7e42495dd33d583f2747656
Ethan <ethan.gantt@stu.barren.kyschools.us>
2024-04-08 10:19:40 -0500

    Finished Exercise

---
+++ script.js
@@ -0,0 +1,24 @@
+const jPDinos = [
+    "Velociraptors",
+     "Tyrannosaurus Rex",
+     "Dilophosaurus"
+];
+
+function seeDinos(a, b, c){
+    console.log(a);
+    console.log(``);
+    console.log(b);
+    // console.log(c);
+
+}
+
+seeDinos(...jPDinos)
+
+const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+}
+
+seeCharacters = () => `Laura Dern`
+console.log(seeCharacters());
